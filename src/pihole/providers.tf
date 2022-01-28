d0056209e53e93b00b68518dda840f3930d40f66
Andrew Shoell <mrlunchbox777@gmail.com>
2022-01-27 23:01:19 -0700

    fixing providers call

--- src/pihole/providers.tf
+++ src/pihole/providers.tf
@@ -1,11 +1,12 @@
 terraform {
   required_providers {
-    helm {
+    helm = {
       source = "hashicorp/helm"
-    }
-    kubernetes {
-      source  = "hashicorp/kubernetes"
       version = ">= 2.4"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.7"
     }
   }
 }
