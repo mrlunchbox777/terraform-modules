72645a17f3dc20a3d830ecf887189dad3dc53086
Andrew Shoell <mrlunchbox777@gmail.com>
2022-02-13 15:23:46 -0700

    hopefully finished the helmchartconfig creation

--- src/helmchartconfig/generatebase/README.md
+++ src/helmchartconfig/generatebase/README.md
@@ -5,3 +5,17 @@ This is designed to create a helm release config with some generally good defaul
 This can provide a good way to generate a base config for downstream projects.
 
 See [the helmchartconfig](/src/helmchartconfig/) for more details.
+
+## Examples
+
+Here is an example on how to use helmchartconfig/generatebase
+
+```terraform
+module "config" {
+  source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helmchartconfig/generatebase?ref=helmchartconfig/generatebase/1.0.0"
+
+  name                       = var.name
+  namespace                  = var.namespace
+  chart                      = var.chart
+}
+```
--- src/helmchartconfig/main.tf
+++ src/helmchartconfig/main.tf
@@ -1,14 +1,22 @@
 locals {
-  helmchartconfig = { for k, v in local.initialmap :
+  helmchartconfig = {for k, v in local.initialmap :
     k => (k == "values" || k == "set" || k == "set_sensitive" || k == "postrender"
-			? merge(
-				coalescelist(local.initialmap[k], []),
-				coalescelist(local.basemap[k],[]),
-				coalescelist(local.overridemap[k],[]),
-				coalescelist(local.parametersmap[k],[]),
+			? concat(
+				try(coalescelist(local.initialmap[k], []), []),
+				try(coalescelist(local.basemap[k], []), []),
+				try(coalescelist(local.overridemap[k], []), []),
+				try(coalescelist(local.parametersmap[k], []), [])
 			)
-			:
-			lookup(local.parametersmap, k, null)
+			: (lookup(local.parametersmap, k, null) != null
+				? lookup(local.parametersmap, k, null)
+				: (lookup(local.overridemap, k, null) != null
+					? lookup(local.overridemap, k, null)
+					: (lookup(local.basemap, k, null) != null
+						? lookup(local.basemap, k, null)
+						: lookup(local.initialmap, k, null)
+					)
+				)
+			)
     )
   }
 }
