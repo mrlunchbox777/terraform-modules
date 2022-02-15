e8040999f790e51cff5b6ceca8fe9cf78e1dacb4
Andrew Shoell <mrlunchbox777@gmail.com>
2022-02-14 23:38:52 -0700

    WIP adding a slurp

--- src/metallb/get_kind_ip_range.sh
+++ src/metallb/get_kind_ip_range.sh
@@ -7,7 +7,7 @@ all_subnets=$(docker network inspect $network_name | jq '[.[] | .IPAM.Config | .
 # echo $all_subnets
 ipv4_subnets=$(echo $all_subnets | jq '[.[] | select(test("^([0-9]{1,3}\\.){3}[0-9]{1,3}"))]')
 # echo $ipv4_subnets
-ips_and_cidrs=$(echo $ipv4_subnets | jq -r '.[]' | xargs -I % sh -c "echo % | sed 's|^\(.*\)/\(.*\)|{\"ips\": \"\1\", \"cidr\": \"\2\"}|g'" | jq .)
+ips_and_cidrs=$(echo $ipv4_subnets | jq -r '.[]' | xargs -I % sh -c "echo % | sed 's|^\(.*\)/\(.*\)|{\"ips\": \"\1\", \"cidr\": \"\2\"}|g'" | jq -s .)
 
 echo $ips_and_cidrs
 # jq -n --arg foobaz "$FOOBAZ" '{"foobaz":$foobaz}'
