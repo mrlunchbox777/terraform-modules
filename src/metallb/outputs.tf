87b5c96741cbe247a0eb6112f2ca32936b07d1fe
Andrew Shoell <mrlunchbox777@gmail.com>
2022-02-21 20:53:58 -0700

    adding the secret to the outputs and moving it to it's own file

--- src/metallb/outputs.tf
+++ src/metallb/outputs.tf
@@ -7,3 +7,8 @@ output "configmap" {
   value       = kubernetes_config_map.metallb_config_map
   description = "configmap for metallb"
 }
+
+output "memberlist_secret" {
+  value       = kubernetes_secret.metallb_memberlist_secret
+  description = "the memberlist secret"
+}
---
+++ src/metallb/secrets.tf
@@ -0,0 +1,20 @@
+resource "random_id" "metallb_secret_key" {
+  keepers = {
+    helm_revision = module.metallb.release.metadata.revision
+  }
+
+  byte_length = 128
+}
+
+resource "kubernetes_secret" "metallb_memberlist_secret" {
+  metadata {
+    annotations = var.config.config_map_annotations
+    labels      = var.config.config_map_labels
+    name        = "${var.config.helm_release_config.name}-memberlist"
+    namespace   = var.config.helm_release_config.namespace
+  }
+
+  binary_data = (length(var.config.memberlist_secret_override) > 0
+    ? var.config.memberlist_secret_override
+    : random_id.metallb_secret_key.id)
+}
