d018b6bc19796ac306ce0514a344ace609999ce2
Andrew Shoell <mrlunchbox777@gmail.com>
2022-02-21 22:31:48 -0700

    updating the helm release

--- src/metallb/main.tf
+++ src/metallb/main.tf
@@ -38,7 +38,7 @@ resource "kubernetes_config_map" "metallb_config_map" {
 }
 
 module "metallb" {
-  source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helmrelease?ref=helmrelease/0.0.4"
+  source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helm_release?ref=helm_release/0.0.4"
 
   config = var.config.helm_release_config
 }
