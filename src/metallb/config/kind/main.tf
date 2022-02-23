a5ba402c88ac95d78837055156d2de691d31d8bf
Andrew Shoell <mrlunchbox777@gmail.com>
2022-02-22 23:16:34 -0700

    moving network name to the right module

--- src/metallb/config/kind/main.tf
+++ src/metallb/config/kind/main.tf
@@ -11,6 +11,8 @@ locals {
 module "get_ip" {
   # source = "git::https://github.com/mrlunchbox777/terraform-modules//src/metallb/config/get_ip?ref=metallb/config/get_ip/0.0.1"
   source = "git::https://github.com/mrlunchbox777/terraform-modules//src/metallb/config/get_ip?ref=bug/fixing-errors"
+
+  network_name                  = var.network_name
 }
 
 module "config" {
@@ -21,6 +23,5 @@ module "config" {
   config_map_annotations        = var.config_map_annotations
   config_map_labels             = var.config_map_labels
   memberlist_secret_override    = var.memberlist_secret_override
-  network_name                  = var.network_name
   config_map_data_address_pools = local.kind_address_pools
 }
