e7689f003132e6fcfe3b06cb333f0d891d42ab17
Andrew Shoell <mrlunchbox777@gmail.com>
2022-02-22 17:46:16 -0700

    forgot to pin the release configs

--- src/metallb/config/helm_release_configs.tf
+++ src/metallb/config/helm_release_configs.tf
@@ -1,5 +1,6 @@
 module "base_config" {
-  source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helm_release/config/base?ref=helm_release/config/base/0.0.2"
+  # source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helm_release/config/base?ref=helm_release/config/base/0.0.2"
+  source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helm_release/config/base?ref=bug/fixing-errors"
 
   name      = local.name
   namespace = local.namespace
@@ -7,7 +8,8 @@ module "base_config" {
 }
 
 module "config" {
-  source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helm_release/config/base?ref=helm_release/config/0.0.2"
+  # source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helm_release/config/base?ref=helm_release/config/0.0.2"
+  source = "git::https://github.com/mrlunchbox777/terraform-modules//src/helm_release/config/base?ref=bug/fixing-errors"
 
   base_config     = module.base_config.config
   override_config = var.override_config
