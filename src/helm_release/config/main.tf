c87768b5add55d38d1f19e186ec508ca3ae55525
Andrew Shoell <mrlunchbox777@gmail.com>
2022-02-23 23:08:49 -0700

    trying a different way to do the merging of configs

--- src/helm_release/config/main.tf
+++ src/helm_release/config/main.tf
@@ -1,29 +1,35 @@
 locals {
-  config_for_lists = { for k, v in local.initialmap :
-    k => (k == "values" || k == "set" || k == "set_sensitive" || k == "postrender"
-      ? concat(
-        try(coalescelist(local.initialmap[k], []), []),
-        try(coalescelist(local.basemap[k], []), []),
-        try(coalescelist(local.overridemap[k], []), []),
-        try(coalescelist(local.parametersmap[k], []), [])
-      )
-      : []
+  collection_props = [
+    "values",
+    "set",
+    "set_sensitive",
+    "postrender"
+  ]
+
+  config_for_lists = { for k in local.collection_props :
+    k => concat(
+      try(coalescelist(local.initialmap[k], []), []),
+      try(coalescelist(local.basemap[k], []), []),
+      try(coalescelist(local.overridemap[k], []), []),
+      try(coalescelist(local.parametersmap[k], []), [])
     )
   }
 
-  config = { for k, v in local.initialmap :
-    k => (k == "values" || k == "set" || k == "set_sensitive" || k == "postrender"
-      ? lookup(local.config_for_lists, k, null)
-      : (lookup(local.parametersmap, k, null) != null
-        ? lookup(local.parametersmap, k, null)
-        : (lookup(local.overridemap, k, null) != null
-          ? lookup(local.overridemap, k, null)
-          : (lookup(local.basemap, k, null) != null
-            ? lookup(local.basemap, k, null)
-            : lookup(local.initialmap, k, null)
-          )
+  config_temp = { for k, v in local.initialmap :
+    k => contains(local.collection_props, k)
+    ? lookup(local.initialmap, k, null)
+    : (lookup(local.parametersmap, k, null) != null
+      ? lookup(local.parametersmap, k, null)
+      : (lookup(local.overridemap, k, null) != null
+        ? lookup(local.overridemap, k, null)
+        : (lookup(local.basemap, k, null) != null
+          ? lookup(local.basemap, k, null)
+          : lookup(local.initialmap, k, null)
         )
       )
     )
+  }
+
+    config = merge(config_temp, config_for_lists)
   }
 }
