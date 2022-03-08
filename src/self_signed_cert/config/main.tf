fa51e5a8343876a3f195460987463da9ff39a8ca
Andrew Shoell <mrlunchbox777@gmail.com>
2022-03-07 22:03:22 -0700

    WIP: updated a few things to move over to allow a wrapper around all of the cert resources

--- src/self_signed_cert/config/main.tf
+++ src/self_signed_cert/config/main.tf
@@ -10,17 +10,32 @@ locals {
       private_key_pem = ""
     }
     self_signed_cert_config = {
-      subject = {
-        common_name = ""
-        organization = ""
-        organizational_unit = ""
-        street_address = [""]
-        locality = ""
-        province = ""
-        country = ""
-        postal_code = ""
-        serial_number = ""
+      cert_request = {
+        subject = {
+          common_name = ""
+          organization = ""
+          organizational_unit = ""
+          street_address = [""]
+          locality = ""
+          province = ""
+          country = ""
+          postal_code = ""
+          serial_number = ""
+        }
+        dns_names = [""]
+        ip_addresses = [""]
+        uris = [""]
       }
+      validity_period_hours = 2160
+      allowed_uses = [
+        "key_encipherment",
+        "digital_signature",
+        "server_auth",
+        "client_auth"
+      ]
+      early_renewal_hours = 1440
+      is_ca_certificate = false
+      set_subject_key_id = false
     }
   }
 }
--- src/self_signed_cert/main.tf
+++ src/self_signed_cert/main.tf
@@ -20,15 +20,23 @@ resource "tls_self_signed_cert" "self_signed_cert" {
   private_key_pem = local.private_key_pem
 
   subject {
-    common_name  = "example.com"
-    organization = "ACME Examples, Inc"
+    common_name = var.config.self_signed_cert_config.cert_request.subject.common_name
+    organization = var.config.self_signed_cert_config.cert_request.subject.organization
+    organizational_unit = var.config.self_signed_cert_config.cert_request.subject.organizational_unit
+    street_address = var.config.self_signed_cert_config.cert_request.subject.street_address
+    locality = var.config.self_signed_cert_config.cert_request.subject.locality
+    province = var.config.self_signed_cert_config.cert_request.subject.province
+    country = var.config.self_signed_cert_config.cert_request.subject.country
+    postal_code = var.config.self_signed_cert_config.cert_request.subject.postal_code
+    serial_number = var.config.self_signed_cert_config.cert_request.subject.serial_number
   }
 
-  validity_period_hours = 12
-
-  allowed_uses = [
-    "key_encipherment",
-    "digital_signature",
-    "server_auth",
-  ]
+  validity_period_hours = var.config.self_signed_cert_config.validity_period_hours
+  allowed_uses = var.config.self_signed_cert_config.allowed_uses
+  dns_names = var.config.self_signed_cert_config.cert_request.dns_names
+  ip_addresses = var.config.self_signed_cert_config.cert_request.ip_addresses
+  uris = var.config.self_signed_cert_config.cert_request.uris
+  early_renewal_hours = var.config.self_signed_cert_config.early_renewal_hours
+  is_ca_certificate = var.config.self_signed_cert_config.is_ca_certificate
+  set_subject_key_id = var.config.self_signed_cert_config.set_subject_key_id
 }
