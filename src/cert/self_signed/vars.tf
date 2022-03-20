d690ae8efb39ed3bcec0dff16d4eabe132dc7f35
Andrew Shoell <mrlunchbox777@gmail.com>
2022-03-19 22:25:30 -0600

    WIP updating some vars

--- src/cert/self_signed/vars.tf
+++ src/cert/self_signed/vars.tf
@@ -1,5 +1,32 @@
 variable "config" {
   type = object({
+    key_override = object({
+      key_algorithm = string
+      private_key_pem = string
+    })
+    self_signed_cert_config = object({
+      cert_request = object({
+        subject = object({
+          common_name = string
+          organization = string
+          organizational_unit = string
+          street_address = list(string)
+          locality = string
+          province = string
+          country = string
+          postal_code = string
+          serial_number = string
+        })
+        dns_names = list(string)
+        ip_addresses = list(string)
+        uris = list(string)
+      })
+      validity_period_hours = number
+      allowed_uses = list(string)
+      early_renewal_hours = number
+      is_ca_certificate = bool
+      set_subject_key_id = bool
+    })
   })
-  description = "Pass an map that matches a scaffold from https://github.com/mrlunchbox777/terraform-modules/tree/main/src/self_signed_cert/config"
+  description = "Pass an map that matches a scaffold from https://github.com/mrlunchbox777/terraform-modules/tree/main/src/cert/config"
 }
